a706daca2a0e9d48b19e8a6b7afba1d8569a0444
carlos13suarez <charliesuarez10@gmail.com>
2024-12-11 19:30:13 -0500

    task 3 eslint fix 1

--- ES6_data_manipulation/3-get_ids_sum.js
+++ ES6_data_manipulation/3-get_ids_sum.js
@@ -1,5 +1,4 @@
-const getStudentIdsSum = (getListStudents) => getListStudents.reduce(
-  (accumulator, item) => accumulator + item.id, 0,
-);
+const getStudentIdsSum = (getListStudents) => getListStudents
+  .reduce((accumulator, item) => accumulator + item.id, 0);
 
 export default getStudentIdsSum;
