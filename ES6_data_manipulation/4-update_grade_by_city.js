4a05407bd61c043b57b90215e7aa80cf3f7be97e
carlos13suarez <charliesuarez10@gmail.com>
2024-12-12 19:17:32 -0500

    task 4 fix 1: use find

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,11 +1,14 @@
 const updateStudentGradeByCity = (getListStudents, city, newGrades) => getListStudents
-	.filter(student => student.location === city)
-	.map(student => {
-		const studentGrade = {
-			...student,
-			grade: newGrades.filter(grade => grade.studentId === student.id).at(0)?.grade ?? "N/A"
-		}
-		return studentGrade;
-	})
+  .filter((student) => student.location === city)
+  .map((student) => {
+    const gradeInfo = newGrades.find((grade) => grade.studentId === student.id);
+
+    const studentGrade = {
+      ...student,
+      grade: gradeInfo ? gradeInfo.grade : 'N/A',
+    };
+
+    return studentGrade;
+  });
 
 export default updateStudentGradeByCity;
