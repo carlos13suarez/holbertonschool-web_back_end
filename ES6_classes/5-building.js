6b240db0e6247644c4dc0e592a975345eab3facb
carlos13suarez <charliesuarez10@gmail.com>
2024-12-15 20:33:20 -0500

    task 5 fix 0: check inside constructor if is not the class itself and evacuation exists as function

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,17 +1,16 @@
-/* eslint-disable class-methods-use-this */
 export default class Building {
   constructor(sqft) {
     if (typeof sqft !== 'number') {
       throw new TypeError('sqft must be a number');
     }
     this._sqft = sqft;
+
+    if (this.constructor !== Building && typeof this.evacuationWarningMessage !== 'function') {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
   }
 
   get sqft() {
     return this._sqft;
-  }
-
-  evacuationWarningMessage() {
-    throw new Error('Class extending Building must override evacuationWarningMessage');
   }
 }
