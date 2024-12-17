a7c74fc7f85b8c4d706bd2cbfc5c48d62ddd2305
carlos13suarez <charliesuarez10@gmail.com>
2024-12-16 20:08:16 -0500

    task 8

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,7 @@
 const cleanSet = (set, startString) => {
   const arr = Array.from(set);
   const modifiedString = arr.map((element) => {
-    if (startString && element.startsWith(startString)) {
+    if (startString && element && element.startsWith(startString)) {
       return element.slice(element.indexOf(startString) + startString.length);
     }
     return '';
