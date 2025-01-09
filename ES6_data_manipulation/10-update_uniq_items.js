38aeb9d0eadbb49fa638f526311619b2b11eeff0
carlos13suarez <charliesuarez10@gmail.com>
2025-01-09 10:32:29 -0500

    task 10

---
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,15 @@
+const updateUniqueItems = (groceryMap) => {
+  if (!(groceryMap instanceof Map)) {
+    throw new Error('Cannot process');
+  }
+
+  groceryMap.forEach((value, key) => {
+    if (value === 1) {
+      groceryMap.set(key, 100);
+    }
+  });
+
+  return groceryMap;
+};
+
+export default updateUniqueItems;
