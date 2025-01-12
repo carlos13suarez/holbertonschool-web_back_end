59b0c59c966d9e389b6bb4a8058b9c01bb1cfcda
carlos13suarez <charliesuarez10@gmail.com>
2025-01-11 19:37:07 -0500

    task 6 fix 2: convert error object to string

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -16,7 +16,7 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
       }
       return {
         status: result.status,
-        value: result.reason,
+        value: result.reason.toString(),
       };
     }));
 }
