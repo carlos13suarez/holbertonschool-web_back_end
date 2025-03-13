7398ec340b5ab998d514a9931582aabdefcaa85b
carlos13suarez <charliesuarez10@gmail.com>
2025-03-13 18:55:15 -0500

    task 0 fix 1: add documentation to module

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+"""This module contains a function to add two floating numbers"""
+
+
 def add(a: float, b: float) -> float:
     """Add two floating numbers"""
     return a + b
