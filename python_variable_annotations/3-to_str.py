55734412d71a31eea0bb6d728f63d1bea5e02000
carlos13suarez <charliesuarez10@gmail.com>
2025-03-13 19:05:52 -0500

    task 3

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""This module contains a function to convert a number to a string"""
+
+
+def to_str(n: float) -> str:
+    """Convert a number to a string"""
+    return str(n)
