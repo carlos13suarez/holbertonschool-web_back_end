f96ec24345c3e15baa06a4541e0fdc1c3fbc9a55
carlos13suarez <charliesuarez10@gmail.com>
2025-03-13 19:37:44 -0500

    task 5 fix 0: annotate argument with typing.List

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """This module contains a function to sum a list of floats"""
+import typing
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: typing.List[float]) -> float:
     """Sum a list of floats"""
     return sum(input_list)
