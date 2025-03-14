315e55550eb63ce05c07e9bff986a374357953a2
carlos13suarez <charliesuarez10@gmail.com>
2025-03-13 21:58:19 -0500

    task 8 pycodestyle compliance

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -2,6 +2,7 @@
 """This module contains a function to make a multiplier"""
 import typing
 
+
 def make_multiplier(multiplier: float) -> typing.Callable[[float], float]:
     """This function makes a multiplier"""
     return lambda x: x * multiplier
