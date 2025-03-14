cdb58014660342b6881d981ebd1c6498870f8017
carlos13suarez <charliesuarez10@gmail.com>
2025-03-13 19:28:18 -0500

    task 5

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""This module contains a function to sum a list of floats"""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """Sum a list of floats"""
+    return sum(input_list)
