7aa688470af571e7ef81837e114290c429ccb441
carlos13suarez <charliesuarez10@gmail.com>
2025-03-13 21:57:44 -0500

    task 8 fix: use Callable from typing module

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 """This module contains a function to make a multiplier"""
-from collections.abc import Callable
+import typing
 
-
-def make_multiplier(multiplier: float) -> Callable[[float], float]:
+def make_multiplier(multiplier: float) -> typing.Callable[[float], float]:
     """This function makes a multiplier"""
     return lambda x: x * multiplier
