28945e5b0db95b2dad28d518c970c8e98ac0390a
carlos13suarez <charliesuarez10@gmail.com>
2025-03-17 18:08:42 -0500

    task 9 pycodestyle

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -2,6 +2,8 @@
 """This module contains a function to get the length of an element"""
 import typing
 
-def element_length(lst: typing.Iterable[typing.Sequence]) -> typing.List[typing.Tuple[typing.Sequence, int]]:
+
+def element_length(lst: typing.Iterable[typing.Sequence]) -> typing.List[
+        typing.Tuple[typing.Sequence, int]]:
     """This function gets the length of an element"""
     return [(i, len(i)) for i in lst]
