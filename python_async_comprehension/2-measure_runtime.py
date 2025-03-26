fc0de60ad5d0e8ca20c0682d0f3c6dd912e00df4
carlos13suarez <charliesuarez10@gmail.com>
2025-03-25 19:11:05 -0500

    task 2 fix: use list comprehension with dereference in asyncio.gather

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -12,11 +12,6 @@ async def measure_runtime() -> float:
     Measures total runtime of executing async_comprehension 4 times in parallel
     """
     start_time = time.time()
-    await asyncio.gather(
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension()
-    )
+    await asyncio.gather(*[async_comprehension() for _ in range(4)])
     end_time = time.time()
     return end_time - start_time
