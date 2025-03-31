91024ad1a60750ce622f2cc4a0deaec5c56dea4c
carlos13suarez <charliesuarez10@gmail.com>
2025-03-30 20:26:46 -0500

    task 1

---
+++ pagination/1-simple_pagination.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env/python3
+"""Module for simple pagination"""
+
+import csv
+import math
+from typing import List, Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """
+    This function calculates the start and end indexes
+    for paginating a dataset.
+
+    Args:
+        page (int): The current page number (1-based index).
+        page_size (int): The number of items per page.
+
+    Returns:
+        tuple: A tuple containing the start index and end index
+        for slicing the dataset.
+    """
+    start = (page - 1) * page_size
+    end = page * page_size
+
+    index_tuple = (start, end)
+
+    return index_tuple
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """
+        This function returns a specific page of data from the dataset.
+        Args:
+            page (int): The current page number (1-based index).
+            page_size (int): The number of items per page.
+
+        Returns:
+            list: A list of lists containing the data for the specified page.
+        """
+        assert isinstance(page, int) and isinstance(page_size, int)
+        assert page > 0 and page_size > 0
+
+        start, end = index_range(page, page_size)
+        return self.dataset()[start:end]
