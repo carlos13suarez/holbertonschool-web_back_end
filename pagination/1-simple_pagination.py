cadc1027ff884500458a49e6f1c5f842e12b699c
carlos13suarez <charliesuarez10@gmail.com>
2025-03-31 22:18:40 -0500

    task 1 try 0

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -58,17 +58,8 @@ class Server:
             list: A list of lists containing the data for the specified page.
         """
 
-        data = self.dataset()
+        assert isinstance(page, int) and isinstance(page_size, int) and page > 0 and page_size > 0
 
-        assert isinstance(page, int) and isinstance(page_size, int)
-        assert page > 0 and page_size > 0
+        start, end = index_range(page, page_size)
 
-        index_to_paginate = index_range(page, page_size)
-
-        if index_to_paginate[0] >= len(data):
-            return []
-
-        start = index_to_paginate[0]
-        end = index_to_paginate[1]
-
-        return list(data[start:end])
+        return self.dataset()[start:end]
