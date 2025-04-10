ef200a25dffabdc9aa1cdde7105ad64cb955ec21
carlos13suarez <charliesuarez10@gmail.com>
2025-04-09 20:21:50 -0500

    task 10

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+    Update topics of a school document based on the name
+"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """Update topics of a school document based on the name"""
+    mongo_collection.update_many(
+        {"name": name},
+        {"$set": {"topics": topics}},
+    )
