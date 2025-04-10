7ed1cab3dfb2c3acdbc3b8f1895e960c6f0a1d48
carlos13suarez <charliesuarez10@gmail.com>
2025-04-09 20:09:02 -0500

    task 8

---
+++ NoSQL/8-all.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+    List all documents in a collection
+"""
+
+
+def list_all(mongo_collection):
+    """List all documents in a collection"""
+    return mongo_collection.find()
