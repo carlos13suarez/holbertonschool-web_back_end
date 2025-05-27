13a113c048124bbfa2e907ee473742146cf7269d
carlos13suarez <charliesuarez10@gmail.com>
2025-05-26 19:50:27 -0500

    task 1 fix 1: add newline to answer

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -3,7 +3,7 @@ process.stdin.setEncoding('utf8');
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('data', (name) => {
-  console.log(`Your name is: ${name}`);
+  console.log(`Your name is: ${name}\n`);
 });
 
 process.stdin.on('end', () => {
