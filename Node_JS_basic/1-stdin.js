6ae6a56a047621980b529865e45270c87bfd4121
carlos13suarez <charliesuarez10@gmail.com>
2025-05-26 19:53:07 -0500

    task 1 try 0: try printing out with stdout.write instead

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -3,9 +3,9 @@ process.stdin.setEncoding('utf8');
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('data', (name) => {
-  console.log(`Your name is: ${name}\n`);
+  process.stdout.write(`Your name is: ${name}`);
 });
 
 process.stdin.on('end', () => {
-  console.log('This important software is now closing\n');
+  process.stdout.write('This important software is now closing\n');
 });
