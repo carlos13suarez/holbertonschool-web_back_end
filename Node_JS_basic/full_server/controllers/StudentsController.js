f287267b58501083eb33b82cff79636c2c222fcb
carlos13suarez <charliesuarez10@gmail.com>
2025-05-31 21:06:03 -0500

    task 8

---
+++ Node_JS_basic/full_server/controllers/StudentsController.js
@@ -0,0 +1,37 @@
+import {readDatabase} from '../utils.js';
+
+class StudentsController {
+  static getAllStudents(req, res) {
+    const path = process.argv[2];
+    readDatabase(path)
+      .then((data) => {
+        let response = 'This is the list of our students';
+        const fields = Object.keys(data).sort();
+        for (const field of fields) {
+          const list = data[field];
+          response += `\nNumber of students in ${field}: ${list.length}. List: ${list.join(', ')}`;
+        }
+        res.status(200).send(response);
+      })
+      .catch(() => res.status(500).send('Cannot load the database'));
+  }
+
+  static getAllStudentsByMajor(req, res) {
+    const path = process.argv[2];
+    const {major} = req.params;
+
+    if (major !== 'CS' && major !== 'SWE') {
+      res.status(500).send('Major parameter must be CS or SWE');
+      return;
+    }
+
+    readDatabase(path)
+      .then((data) => {
+        const list = data[major];
+        res.status(200).send(`List: ${list.join(', ')}`);
+      })
+      .catch(() => res.status(500).send('Cannot load the database'));
+  }
+}
+
+export default StudentsController;
---
+++ Node_JS_basic/full_server/utils.js
@@ -0,0 +1,24 @@
+import fs from 'fs';
+
+export function readDatabase(filePath) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, 'utf8', (err, data) => {
+      if (err) {
+        reject(Error('Cannot load the database'));
+        return;
+      }
+
+      const lines = data.trim().split('\n').filter((line) => line !== '');
+      const students = lines.slice(1);
+      const result = {};
+
+      students.forEach((line) => {
+        const [firstName, , , field] = line.split(',');
+        if (!result[field]) result[field] = [];
+        result[field].push(firstName);
+      });
+
+      resolve(result);
+    });
+  });
+}
